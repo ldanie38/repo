da1b15cef93709cd1614d57524f40ed80a6083b7
AliCelik00 <alifaik2000@hotmail.com>
2025-09-03 02:07:23 -0400

    Implement error handler middleware and logging

--- src/auth_app/auth_service.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-
-def create_user(username: str, password: str, email: str = ""):
-    return User.objects.create(
-        username=username,
-        email=email,
-        password=make_password(password)
-    )
